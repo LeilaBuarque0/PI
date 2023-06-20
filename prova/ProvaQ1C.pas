3a352eabe72eab2a94701b55589d1def83f454f2
Aluno <aluno@ifslab.edu.br>
2023-06-20 16:29:42 -0300

    novo arquivo copia do arquivo 1

---
+++ prova/ProvaQ1C.pas
@@ -0,0 +1,22 @@
+Program ProvaQ1 ;
+function round(5.56:real):real;
+function abs(-3:integer)integer;
+function sqrt(4:real):real;
+function trunc(5,56:real)real;
+function sqr(4:real):real;
+function frac(5,56:real):real;
+
+
+Begin
+A:=2;
+B:=5.56;
+C:=-3;
+D:=4;
+
+
+
+
+
+
+  
+End.
